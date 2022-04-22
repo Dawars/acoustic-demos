9c66bcef8c0a4f76e45da0237115a8dea2752b7f
Dávid Komorowicz <dawars00@gmail.com>
2022-04-22 23:32:47 +0200

    Initial app with gui

---
+++ include/acoustics/eigen_frequency.h
@@ -0,0 +1,9 @@
+//
+// Created by dawars on 2022. 04. 22..
+//
+
+#pragma once
+class eigen_frequency {
+public:
+    void test();
+};
---
+++ src/eigen_frequency.cpp
@@ -0,0 +1,12 @@
+//
+// Created by dawars on 2022. 04. 22..
+//
+
+#include "acoustics/eigen_frequency.h"
+
+#define TETLIBRARY
+#include "../libs/tetgen/tetgen.h"
+
+void eigen_frequency::test(){
+//    tetrahedralize();
+}
